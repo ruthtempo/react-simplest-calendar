afa2fb7abf4f44c7af89b74bc590642054d37e65
ruthtempo <ruthtemp@gmail.com>
2022-07-05 17:40:26 +0200

    customization with props and demo with bootstrap

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,29 +1,41 @@
+import { format, weeksToDays } from "date-fns";
 import React from "react";
-import { render } from "react-dom";
 import ReactDOM from 'react-dom/client';
 import { Calendar } from '../src'
-
+import { Button, Table } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const App = () => {
   return (
     <>
-      <h1>
-        React Calendar
+      <h1 className="text-center py-4">
+        React Simple Calendar
       </h1>
       <Calendar
+        weekStartsOn={1}
+        weekDayLabels={["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]}
         renderWeekday={(weekday) =>
-          <td style={{ fontSize: "8px" }}>{weekday.toLocaleLowerCase()}</td>
+          <td key={weekday} className="text-end">{weekday}</td>
         }
         renderDay={(day) =>
           <td
             key={day.day.toISOString()}
-            style={{
-              fontWeight: day.isCurrentDate ? "bold" : "normal",
-              color: day.isCurrentMonth ? "black" : "lightgray"
-            }}
+            className={`${day.isCurrentDate ? "fw-bold" : ""} text-end ${!day.isCurrentMonth ? "text-muted" : ""} `}
           >
             {day.day.getDate()}
           </td>
+        }
+        renderMonthTitle={(currentDate, prevMonth, nextMonth) => (
+          <div className="d-flex justify-content-between">
+            <Button onClick={prevMonth}>prev</Button>
+            <h3>{format(currentDate, 'MMMM yyyy')}</h3>
+            <Button onClick={nextMonth} >next</Button>
+          </div>
+        )}
+        renderTable={(children) =>
+          <Table style={{ width: 500 }}>
+            {children}
+          </Table>
         }
       />
     </>
--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,20 @@ import { differenceInCalendarDays, endOfMonth, endOfWeek, getWeeksInMonth, isSam
 import { add, startOfMonth, sub, getDate, startOfWeek } from "date-fns/esm";
 import format from "date-fns/format";
 
+type WeekStartsOn = 0 | 1 | 2 | 3 | 4 | 5 | 6
+
 interface CalendarRecord {
   day: Date,
   isCurrentDate: boolean,
   isCurrentMonth: boolean
 }
 
-function getDaysArray(selectedMonth: Date): CalendarRecord[] {
+function getDaysArray(selectedMonth: Date, weekStartsOn: WeekStartsOn): CalendarRecord[] {
   const start = startOfMonth(selectedMonth)
-  const begginingOfWeek = startOfWeek(start, { weekStartsOn: 1 })
+  const begginingOfWeek = startOfWeek(start, { weekStartsOn })
 
   const end = endOfMonth(selectedMonth)
-  const endOfTheWeek = endOfWeek(end, { weekStartsOn: 1 })
+  const endOfTheWeek = endOfWeek(end, { weekStartsOn })
 
   const daysCount = differenceInCalendarDays(endOfTheWeek, begginingOfWeek)
   const daysInMonth: CalendarRecord[] = []
@@ -31,8 +33,12 @@ function getDaysArray(selectedMonth: Date): CalendarRecord[] {
   return daysInMonth
 }
 
-function getWeeks(days: CalendarRecord[], currentDate: Date): CalendarRecord[][] {
-  const numberOfWeeks = getWeeksInMonth(currentDate, { weekStartsOn: 1 })
+function getWeeks(
+  days: CalendarRecord[],
+  currentDate: Date,
+  weekStartsOn: WeekStartsOn
+): CalendarRecord[][] {
+  const numberOfWeeks = getWeeksInMonth(currentDate, { weekStartsOn })
   const month: CalendarRecord[][] = []
   let counter = 0
   //weeks
@@ -58,15 +64,36 @@ const defaultRenderDay = (day: CalendarRecord): JSX.Element =>
     {getDate(day.day)}
   </td>
 
+const defaultMonthTitle = (
+  currentDate: Date,
+  prevMonth: () => void,
+  nextMonth: () => void,
+): JSX.Element => (
+  <div style={{ display: 'flex' }}>
+    <button onClick={prevMonth}>prev</button>
+    <h3>{format(currentDate, 'MMMM yyyy')}</h3>
+    <button onClick={nextMonth} >next</button>
+  </div>
+)
+
+const defaultRenderTable = (children: JSX.Element) =>
+  <table>
+    {children}
+  </table>
+
 export const Calendar = (p: {
   renderWeekday?: typeof defaultRenderWeekday,
   renderDay?: typeof defaultRenderDay,
+  renderMonthTitle?: typeof defaultMonthTitle,
+  renderTable?: typeof defaultRenderTable,
+  weekStartsOn?: WeekStartsOn,
+  weekDayLabels?: string[]
 }) => {
-  const weekdays = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
+  const weekdays = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
 
   const [currentDate, setCurrentDate] = useState(new Date())
-  const daysInMonth = getDaysArray(currentDate)
-  const rows = getWeeks(daysInMonth, currentDate)
+  const daysInMonth = getDaysArray(currentDate, p.weekStartsOn ?? 1)
+  const rows = getWeeks(daysInMonth, currentDate, p.weekStartsOn ?? 1)
 
   function nextMonth() {
     setCurrentDate(add(currentDate, { months: 1 }))
@@ -75,31 +102,30 @@ export const Calendar = (p: {
   function prevMonth() {
     setCurrentDate(sub(currentDate, { months: 1 }))
   }
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th colSpan={7}>
-            <div >
-              <button onClick={prevMonth}>prev</button>
-              {format(currentDate, 'MMMM yyyy')}
-              <button onClick={nextMonth} >next</button>
-            </div>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          {weekdays.map(p.renderWeekday ?? defaultRenderWeekday)}
-          {/* <td key={weekday}><small>{weekday}</small></td> */}
-        </tr>
-        {rows.map(row => (
-          <tr key={row[0].day.toISOString()} >
-            {row.map(p.renderDay ?? defaultRenderDay)}
-          </tr>
-        ))}
-      </tbody>
-    </table>
 
+  return (
+    (p.renderTable ?? defaultRenderTable)(
+      <>
+        <thead>
+          <tr>
+            <th colSpan={7}>
+              {(p.renderMonthTitle ?? defaultMonthTitle)(currentDate, prevMonth, nextMonth)}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {(p.weekDayLabels ?? weekdays).map(p.renderWeekday ?? defaultRenderWeekday)}
+            {/* <td key={weekday}><small>{weekday}</small></td> */}
+          </tr>
+          {rows.map(row => (
+            <tr key={row[0].day.toISOString()} >
+              {row.map(p.renderDay ?? defaultRenderDay)}
+              {/* {row.map(day => (p.renderDay ?? defaultRenderDay)(day))} */}
+            </tr>
+          ))}
+        </tbody>
+      </>
+    )
   )
 }
